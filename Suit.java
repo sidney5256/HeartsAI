be216b62bfc194fc72a9ee35541db9f7585eeb21
sidney5256 <sep394@nyu.edu>
2019-06-09 13:41:36 -0400

    did reordering of suits

---
+++ Suit.java
@@ -0,0 +1,12 @@
+public enum Suit { CLUBS, DIAMONDS, SPADES, HEARTS }
+
+/*
+How to compare enums:
+Suit one = Suit.HEARTS;
+Suit two = Suit.SPADES;
+// compareTo will be positive if the caller is larger than the parameter
+// otherwise it will be negative
+if (one.compareTo(two) < 0) {
+	System.out.println("this will be printed because one is less than two");
+}
+*/
